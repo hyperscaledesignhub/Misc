d0595b1d58e94ed9d940de1d51c41a1c8d29728e
hyperscaledesignhub <thefreedomcodex@gmail.com>
2026-01-25 07:45:47 +0530

    immutable permute

---
+++ Recursion/practise-1/04-permutation-immutable.py
@@ -0,0 +1,23 @@
+def permutation_immutable(S:list[int]):
+    
+    result = []
+    
+    def helper(i:int):
+        #Base case
+        if i == len(S):
+            result.append(S[:])
+            return
+        
+        #Recursive case
+        for k in range(i,len(S)):
+            S[i],S[k]=S[k],S[i]
+            helper(i+1)
+            S[i],S[k] = S[k],S[i]
+    helper(0)
+    return result
+input=[1,2,3]
+r=permutation_immutable(input)
+print(f"For the given input {input} output is {r}")
+
+        
+        
